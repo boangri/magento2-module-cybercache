a60b9fc8d115d9ddfc810898f51115d9939211c5
Boris Gribovskiy <boris.gribovskiy@alpineinc.com>
2019-11-07 15:05:17 +0300

    Initial public commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is a part of the implementation of the CyberCache Cluster.
+ * Written by Vadim Sytnikov.
+ * Copyright (C) 2016-2019 CyberHULL, Ltd.
+ * All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CyberHULL_CyberCache',
+    __DIR__
+);
